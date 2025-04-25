d3d46cd74d074a4b94549cc4971b512531666bc8
GyanRibeiro <gyanribeiro29@gmail.com>
2025-04-25 16:16:05 -0300

    feat: add currency conversion service

--- src/main/java/com/nayg/Service/ConverterMoedaService.java
+++ src/main/java/com/nayg/Service/ConverterMoedaService.java
@@ -1,4 +1,14 @@
 package com.nayg.Service;
 
-public class ConverterMoeda {
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class ConverterMoedaService {
+
+    public static Double converter(String json, String rate, Double value) {
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        JsonObject rates = jsonObject.getAsJsonObject("conversion_rates");
+
+        return rates.get(rate).getAsDouble() * value;
+    }
 }
